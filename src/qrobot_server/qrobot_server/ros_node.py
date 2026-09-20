7bfbb6f6c77abb5ff2e34571f6616e742b8f562e
Maria Muravieva <maria.muravieva@mail.ru>
2026-09-20 12:10:49 +0000

    Реализована передача видео в сжатом формате

--- src/qrobot_server/qrobot_server/ros_node.py
+++ src/qrobot_server/qrobot_server/ros_node.py
@@ -1,4 +1,5 @@
 import sys
+import zlib
 
 # Qt
 from PyQt6.QtCore import QObject, QTimer, pyqtSignal
@@ -9,8 +10,7 @@ from PyQt6.QtWidgets import QApplication
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from sensor_msgs.msg import CompressedImage
 
 # OpenCV
 import cv2
@@ -28,43 +28,28 @@ class RosNode(Node):
     def __init__(self, qrobot):
         super().__init__('ros_node')
         self.qrobot = qrobot
-        self.cv_bridge = CvBridge()
 
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             history=HistoryPolicy.KEEP_LAST,
-            depth=10
+            depth=1
         )
 
         # Подписка на изображения с камеры
         self.create_subscription(
-            Image,
-            "/camera/image_raw",
+            CompressedImage,
+            "/camera/image_raw/compressed",
             self.image_callback,
             qos_profile
         )
 
     def image_callback(self, msg):
         try:
-            # Convert raw ROS image to an OpenCV BGR image
-            cv_img = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
-            
-            # Convert BGR (OpenCV) to RGB (Qt standard)
-            rgb_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-            h, w, ch = rgb_img.shape
-            bytes_per_line = ch * w
-            
-            # Convert NumPy array to QImage
-            qt_image = QImage(
-                rgb_img.data, 
-                w, 
-                h, 
-                bytes_per_line, 
-                QImage.Format.Format_RGB888
-            )
-            
-            # Emit copy to avoid garbage collection/memory access race conditions
-            self.qrobot.image_received.emit(qt_image.copy())
+            compressed_data = msg.data
+            decompressed_data = zlib.decompress(compressed_data)
+            qt_image = QImage.fromData(decompressed_data)
+            if not qt_image.isNull():
+                self.qrobot.image_received.emit(qt_image)
             
         except Exception as e:
             print(f"Error parsing image: {e}")
@@ -85,7 +70,7 @@ def main(args=None):
     # Use QTimer to spin ROS 2 callbacks inside the Qt event loop
     timer = QTimer()
     timer.timeout.connect(lambda: rclpy.spin_once(ros_node, timeout_sec=0.01))
-    timer.start(10)  # 10ms interval
+    timer.start(5)
 
     # Run Qt application loop
     exit_code = app.exec()
